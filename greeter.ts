680f7cf391177036c7cfc8e5fbb5586ada99e188
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-11-29 12:16:25 +0300

    Set up linting

---
+++ greeter.ts
@@ -0,0 +1,15 @@
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person) {
+    return 'Hello, ' + person;
+}
+
+const user = {
+    firstName: 'Jane',
+    lastName: 'User'
+};
+
+document.body.innerHTML = greeter(user);

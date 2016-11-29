0cf0f25dedd775de76bf7d61f057e4b95e99424a
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-11-29 12:52:27 +0300

    Qick start section of the docs

--- greeter.ts
+++ greeter.ts
@@ -1,15 +1,19 @@
+class Student {
+    public fullName: string;
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+    }
+}
+
 interface Person {
     firstName: string;
     lastName: string;
 }
 
 function greeter(person: Person) {
-    return 'Hello, ' + person;
+    return `Hello, ${person.firstName} ${person.lastName}`;
 }
 
-const user = {
-    firstName: 'Jane',
-    lastName: 'User'
-};
+const user = new Student('Jane', 'M.', 'User');
 
 document.body.innerHTML = greeter(user);
